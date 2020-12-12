25ff85b477c7f7b3fd408bc81ba98c3450f4cf04
armyaali <ali.umar.work@gmail.com>
2020-12-11 19:58:48 -0500

    got youtube music player working using discord-music-player lib

--- bot.js
+++ bot.js
@@ -1,26 +1,64 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs"); // talk to the filesystem on the os
+const config = require("./bot_config.js");
 const { Player } = require("discord-music-player");
-const config = require('./bot_config.js');
-// CONSTANTS
-const generalChannel = '143853351103102976';
-const generalChannelVoice = '143853351103102977';
+const player = new Player(client, {
+    leaveOnEnd: false,
+    leaveOnStop: true,
+    leaveOnEmpty: true,
+    quality: 'high',
+});
 
-client.on('ready', () => {
+client.player = player;
+
+// CONSTANTS (TO DO SHOULD BE IN A SPERPATE FILE)
+const generalChannel = "143853351103102976";
+
+
+// GRAB ALL OUR COMMANDS BEFORE WE LOGIN
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+client.commands = new Discord.Collection();
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
+}
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content.includes('ping')) {
-    msg.reply('pong');
+
+client.on("message", async (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  // !play This is the Life
+  // will play "This is the Life" in the Voice Channel
+
+  if(command === 'play'){
+      const searchString = args.join(' ');
+      let song = await client.player.play(message.member.voice.channel, searchString, {
+          duration: 'long' // This is optional
+      });
+      song = song.song;
+      message.channel.send(`Started playing ${song.name}.`);
   }
 });
 
 // sends a welcome message if a user joins
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.cache.find(ch => ch.id == generalChannel);
+client.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.id == generalChannel
+  );
   if (!channel) return;
-  channel.send('Welcome to the Mushroom Cave' + ' ' + member.displayName + '!');
+  channel.send("Welcome to the Mushroom Cave" + " " + member.displayName + "!");
 });
 
 client.login(config.token);
