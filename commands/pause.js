27167660eef7af3f76691ea82cb739090389d202
armyaali <ali.umar.work@gmail.com>
2020-12-11 21:04:38 -0500

    created a basic pause command to pause music

---
+++ commands/pause.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "pause",
+    description: "pause the currently running music",
+    async execute(player, message) {
+      // check if there is a song already in queue
+      let isPlaying = player.isPlaying(message.guild.id);
+      try {
+        if(!isPlaying) {
+          message.channel.send(`Mushroomie! Nothing is playing...`);
+        } else {
+          let song = await player.pause(message.guild.id);
+          message.channel.send(`${song.name} was paused!`);
+        }
+      } catch(error) {
+        console.log(error)
+      }
+    },
+  };
