054a075565d39f67b49b7319baa09b51c06cd279
armyaali <ali.umar.work@gmail.com>
2020-12-11 20:24:14 -0500

    moved the youtube playing player logic into the play.js command file

--- commands/play.js
+++ commands/play.js
@@ -2,9 +2,22 @@ const { Player } = require("discord-music-player");
 const generalChannelVoice = "143853351103102977";
 
 module.exports = {
-	name: 'play',
-	description: 'play music',
-	execute(args) {
-       
-	},
+  name: "play",
+  description: "play music",
+  async execute(message, args) {
+    const player = new Player(message.client, {
+      leaveOnEnd: false,
+      leaveOnStop: true,
+      leaveOnEmpty: true,
+      quality: "high",
+    });
+    const searchString = args.join(" ");
+    let song = await player.play(message.member.voice.channel, searchString, {
+      duration: "long", // This is optional
+    });
+
+    song = song.song;
+    console.log(song.song);
+    message.channel.send(`Started playing ${song.name}.`);
+  },
 };
