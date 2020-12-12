f0787fdcb8bcb11db53749ac8baff7971489cc3e
armyaali <ali.umar.work@gmail.com>
2020-12-11 21:35:50 -0500

    added the ability to resume songs

---
+++ commands/resume.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "resume",
+    description: "resumes the currently paused music",
+    async execute(player, message) {
+      // check if there is a song already in queue
+      let isPlaying = player.isPlaying(message.guild.id);
+      try {
+        if(!isPlaying) {
+          message.channel.send(`Mushroomie! Nothing is playing...`);
+        } else {
+          let song = await player.resume(message.guild.id);
+          message.channel.send(`${song.name} was resumed!`);
+        }
+      } catch(error) {
+        console.log(error)
+      }
+    },
+  };
