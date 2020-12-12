1644e70ef0c09d3679d108f566192daf5a09c5ed
armyaali <ali.umar.work@gmail.com>
2020-12-11 21:42:04 -0500

    implemented !stop command

---
+++ commands/stop.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "stop",
+    description: "stops the current song and clears the queue",
+    async execute(player, message) {
+      // check if there is a song already in queue
+      let isPlaying = player.isPlaying(message.guild.id);
+      try {
+        if(!isPlaying) {
+          message.channel.send(`Mushroomie! Nothing is playing...`);
+        } else {
+        player.stop(message.guild.id);
+          message.channel.send('Mushroomie.. Stopped playing music and cleared the song queue!');
+        }
+      } catch(error) {
+        console.log(error)
+      }
+    },
+  };
