d74c00135febb5d1854455f46235f7cf28928300
armyaali <ali.umar.work@gmail.com>
2020-12-13 10:53:03 -0500

    moved help message into variable

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,16 +1,18 @@
+const helpTemplate = `
+Commands!
+  - Music Player
+      - !play *songname* -> Plays a song!
+      - !queue -> returns all the songs in the queue listed out for you!
+      - !resume/pause -> plays/resumes the currently playing song
+      - !skip -> skips the current song in the queue
+      - !stop -> Stops playing the song and exits the voice channel
+`;
+
 module.exports = {
     name: "help",
     description: "Gives the user the list of basic commands",
     execute(message) {
       message.channel.send('Here are a list of commands I can currently use!');
-      message.channel.send(`
-      Commands!
-        - Music Player
-            - !play *songname* -> Plays a song!
-            - !queue -> returns all the songs in the queue listed out for you!
-            - !resume/pause -> plays/resumes the currently playing song
-            - !skip -> skips the current song in the queue
-            - !stop -> Stops playing the song and exits the voice channel
-      `);
+      message.channel.send(helpTemplate);
     },
   };
