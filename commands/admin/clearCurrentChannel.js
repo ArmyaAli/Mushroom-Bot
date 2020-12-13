8a70ba3aee4673e9681df162ab47215c14ac23fb
armyaali <ali.umar.work@gmail.com>
2020-12-13 12:12:54 -0500

    added clearchannel command template

---
+++ commands/admin/clearCurrentChannel.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "clearch",
+    description: "Clear the currently wrote to channel",
+    execute(message) {
+      console.log(`Clearing the channel!`);
+    },
+  };
+  
