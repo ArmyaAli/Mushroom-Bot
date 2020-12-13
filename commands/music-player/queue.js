97111e06c2e981220121800264a53ac3f7233df1
armyaali <ali.umar.work@gmail.com>
2020-12-13 10:46:31 -0500

    added a basic queue getter command

---
+++ commands/music-player/queue.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "queue",
+    description: "Gets all music in the current song Queue.",
+    async execute(player, message) {
+        let queue = await player.getQueue(message.guild.id);
+        if(queue) {
+            message.channel.send('Queue:\n'+(queue.songs.map((song, i) => {
+                return `${i === 0 ? 'Now Playing' : `#${i+1}`} : ${song.name}`
+            }).join('\n')));
+        } else {
+            message.channel.send('The queue is empty!');
+        }
+    },
+  };
