43cbaa14135e640635edfecb1c79072d21517b92
armyaali <ali.umar.work@gmail.com>
2020-12-13 10:45:57 -0500

    implemented a basic help message

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,7 +1,16 @@
 module.exports = {
     name: "help",
     description: "Gives the user the list of basic commands",
-    execute(args) {
-
+    execute(message) {
+      message.channel.send('Here are a list of commands I can currently use!');
+      message.channel.send(`
+      Commands!
+        - Music Player
+            - !play *songname* -> Plays a song!
+            - !queue -> returns all the songs in the queue listed out for you!
+            - !resume/pause -> plays/resumes the currently playing song
+            - !skip -> skips the current song in the queue
+            - !stop -> Stops playing the song and exits the voice channel
+      `);
     },
   };
