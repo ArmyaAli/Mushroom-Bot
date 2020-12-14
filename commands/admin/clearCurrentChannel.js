298cbe8e45c951199a146625c01a0967434d78f2
armyaali <ali.umar.work@gmail.com>
2020-12-14 18:19:17 -0500

    removed clearChannel command

--- commands/admin/clearCurrentChannel.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "clearch",
-    description: "Clear the currently wrote to channel",
-    execute(message) {
-      console.log(`Clearing the channel!`);
-    },
-  };
-  
