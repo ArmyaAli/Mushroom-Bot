bb3d2a56c32bfc5dbc3bdc1b700ae211a580e254
armyaali <ali.umar.work@gmail.com>
2020-12-14 14:18:08 -0500

    added permission handler to bot.js file

--- bot.js
+++ bot.js
@@ -26,7 +26,8 @@ function readCommandsRecursive(dir) {
     const Absolute = path.join(dir, file);
     if (fs.statSync(Absolute).isDirectory())
       return readCommandsRecursive(Absolute);
-    else return commandFiles.push(Absolute);
+    else 
+      return commandFiles.push(Absolute);
   });
 }
 
@@ -42,10 +43,16 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
 client.on("message", async (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
+
+  const checkPermissions = (message, commandName) => {
+    let authorPerms = message.member.permissions;
+    return authorPerms.has(client.commands.get(commandName).requiredPermissions)
+  };
 
   switch (command) {
     case "play":
@@ -74,8 +81,17 @@ client.on("message", async (message) => {
       client.commands.get("help").execute(message);
       break;
     case "kick":
+      if(!checkPermissions(message, "kick")) {
+        message.channel.send('You do not have the required permissions to excecute that command!')
+        return;
+      };
       client.commands.get("kick").execute(message);
       break;
+    case "clear":
+      client.commands.get("clear").execute(message, args);
+      break;
+    default:
+      message.channel.send("That is not a valid command");
   }
 });
 
