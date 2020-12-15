1d32498b2b62f9519f4adf60eee8819917b208e3
armyaali <ali.umar.work@gmail.com>
2020-12-14 22:09:58 -0500

    refactored kick command and improved error handling and permissions

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -9,19 +9,23 @@ module.exports = {
    * @param {Message} message
    * @param {String[]} args
    */
-  execute(message) {
+  async execute(message) {
     const mentionedMembers = message.mentions.members;
-    if(mentionedMembers.Size > 1) {
-      message.channel.send('You can only kick one user at a time! Please only mention one person to kick');
-    } else {
-      const memberToKick = mentionedMembers.first();
-      if(memberToKick != null) {
-        memberToKick.kick()
-        .then(() => message.channel.send(`Kicked ${memberToKick.displayName}! What a bad shroom!`))
-        .catch((error) =>{ message.channel.send(`You cannot kick someone with a higher role than you!`) });
+      if(mentionedMembers.Size > 1) {
+        message.channel.send('You can only kick one user at a time! Please only mention one person to kick');
       } else {
-        message.channel.send(`You cannot kick someone that does not exist!`);
-      }
+          try {
+          const memberToKick = mentionedMembers.first();
+          if(memberToKick != null) {
+            await memberToKick.kick()
+            message.channel.send(`Kicked ${memberToKick.displayName}! What a bad shroom!`);
+            
+          } else {
+            message.channel.send(`You cannot kick someone that does not exist within the server!`);
+          }
+    } catch(error) {
+      message.channel.send(`You cannot kick someone with a higher role than you!`);
+    }
       
     }
   }
