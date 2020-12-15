3337fda72977759ebe943620befaf3c0f0901c59
armyaali <ali.umar.work@gmail.com>
2020-12-15 18:03:01 -0500

    added await keyword bon like 13+

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -10,22 +10,22 @@ module.exports = {
    * @param {String[]} args
    */
   async execute(message) {
-    const mentionedMembers = message.mentions.members;
-      if(mentionedMembers.Size > 1) {
+    const mentionedMembers = await message.mentions.members;
+      if(mentionedMembers.size > 1) {
         message.channel.send('You can only kick one user at a time! Please only mention one person to kick');
       } else {
           try {
-          const memberToKick = mentionedMembers.first();
-          if(memberToKick != null) {
-            await memberToKick.kick()
-            message.channel.send(`Kicked ${memberToKick.displayName}! What a bad shroom!`);
-            
-          } else {
-            message.channel.send(`You cannot kick someone that does not exist within the server!`);
+            const memberToKick = mentionedMembers.first();
+            if(memberToKick != null) {
+              await memberToKick.kick()
+              message.channel.send(`Kicked ${memberToKick.displayName}! What a bad shroom!`);
+              
+            } else {
+              message.channel.send(`You cannot kick someone that does not exist within the server!`);
+            }
+          } catch(error) {
+            message.channel.send(`You cannot kick someone with a higher role than you!`);
           }
-    } catch(error) {
-      message.channel.send(`You cannot kick someone with a higher role than you!`);
-    }
       
     }
   }
