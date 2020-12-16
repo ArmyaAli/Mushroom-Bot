4cf38d3c861d3741d0dc01a4ba4d7c0ae032afe5
armyaali <ali.umar.work@gmail.com>
2020-12-15 22:29:53 -0500

    sorted results by thumbs up

--- commands/urbandictionary/udquery.js
+++ commands/urbandictionary/udquery.js
@@ -6,31 +6,29 @@ module.exports = {
   description: "Queries urbandictionary using their public API!",
   async execute(message, args) {
     const searchQuery = args.join(" ");
+    console.log(searchQuery)
     const api = `http://api.urbandictionary.com/v0/define?term=${searchQuery}`;
     try {
       const rawdata = await fetch(api);
       const json = await rawdata.json();
-      const results = json.list.slice(0, 5);
-      if(results.length == 0) {
+
+      if(json.list === undefined || json.list.length === 0)
         throw 'No results found! Try a different query';
-      }
-      const messageList = [];
-      for(let i = 0; i < results.length; ++i) {
+
+        let results = json.list.slice(0, 5);
+        results.sort((a, b) =>  b.thumbs_up - a.thumbs_up)
+        for(let i = 0; i < results.length; ++i) {
         const msgFormat = `Definition
                           ${results[i].definition} 
 
                           Example
                           ${results[i].example}
                         `;
-        messageList.push(new MessageEmbed()
+        await message.channel.send(new MessageEmbed()
         .setTitle(`Word: ${searchQuery}, Thumbsup: ${results[i].thumbs_up}, Thumbsdown: ${results[i].thumbs_down}`)
         .setColor(0xff0000)
         .setURL(results[i].permalink)
         .setDescription(msgFormat));
-      }
-
-      for(const msg of messageList) {
-        message.channel.send(msg);
       }
     } catch(error) {
       message.channel.send(`Error occured: ${error}`);
