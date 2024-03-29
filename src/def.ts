0726e883721b9f4f0953850b7db98737e08c40d4
Ali Umar <ali.umar.work@gmail.com>
2024-03-29 10:03:00 -0400

    add song and update MusicPlayer types

--- src/def.ts
+++ src/def.ts
@@ -1,8 +1,26 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js'
+import { ChatInputCommandInteraction, User, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js'
+import { VoiceConnection, AudioPlayer } from '@discordjs/voice'
 
 export interface Command {
     cooldown: number,
     // NOTE(Ali): For some freakin' reason, data.setName() <- cannot contain capital chars
     data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
     exec: (interaction: ChatInputCommandInteraction) => Promise<void>;
+}
+
+interface song {
+  title: string;
+  link: string;
+  duration: string;
+  requester: User;
+}
+
+export interface MusicPlayer {
+    connection: VoiceConnection;
+    player: AudioPlayer;
+    queue: song[];
+    head?: song;
+    tail?: song;
+    current?: song;
+    playing: boolean;
 }
