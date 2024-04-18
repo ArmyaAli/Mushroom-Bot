c89d7198daf24ab63821e590b53753d2d9f7626f
Ali Umar <ali.umar.work@gmail.com>
2024-04-17 20:32:25 -0400

    add queue command file

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -1,6 +1,7 @@
 import { Command } from "../../def";
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-import { resolveQuery } from "../../music-player-api";
+import { displayQueue } from "../../music-player-api";
+import { __musicMap } from "../../globals";
 
 const command: Command = {
     cooldown: 5,
@@ -8,7 +9,11 @@ const command: Command = {
         .setName('queue')
         .setDescription('Tells the user what the next 5 songs in the queue are and who requested them'),
     exec: async function (interaction: ChatInputCommandInteraction) {
-      
+      const guildId = interaction.guildId;
+      if(guildId === null) return;
+      const player = __musicMap.get(guildId);
+      if(!player) return;
+      displayQueue(interaction, player); 
     }
 
 }
